8dca84c7f2dea1441ee6e3dad7dd4b68d62b0b23
Eneida Koçi <99421177+eneidakoci@users.noreply.github.com>
2023-11-03 09:39:13 +0100

    Update AfishimiRreshtaveArgsC.java

--- src/skedaret/AfishimiRreshtaveArgsC.java
+++ src/skedaret/AfishimiRreshtaveArgsC.java
@@ -1,10 +1,15 @@
 package skedaret;
-
-public class AfishimiRreshtaveArgsC {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class AfishimRreshtashArgs {
+    public static void main(String[] args) {
+        // Kontrollojme nese ka argumente te ndryshme
+        if (args.length == 0) {
+            System.out.println("Nuk ka argumente te dhena.");
+        } else {
+            System.out.println("Argumentet e dhena jane:");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argumenti #" + (i + 1) + ": " + args[i]);
+            }
+        }
+    }
 }
+
